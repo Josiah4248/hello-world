b23dd525c383c19a761e4cc84ce7a62c94c308ea
Josiah Skipper <cartoonist49@gmail.com>
2024-02-09 18:25:38 -0500

    Added more info about author.

--- Helloworld.java
+++ Helloworld.java
@@ -3,5 +3,16 @@ public class Helloworld
    public static void main(String[] args)
    {
       System.out.println("Hello world");
+      
+      //name is a reference varibale
+      String name = "Josiah Skppy";
+      
+      System.out.println(name);
+      
+      //age is a primative variable
+      int age = 21;
+      
+      System.out.println(age);
+      
    }
 }
